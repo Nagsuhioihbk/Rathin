337bae59bf8d65acfe24b847ffa4edda33732289
Aravind <aravindbvadavalliki@gmail.com>
2025-10-31 14:16:51 +0530

    sacd

--- dist/assets/html2canvas-6yxB5SAv.js
+++ dist/assets/html2canvas-6yxB5SAv.js
@@ -1,4 +1,4 @@
-import{n as e}from"./index-6cHeKkdV.js";var t=e(((e,t)=>{
+import{n as e}from"./index-B_uKUR_p.js";var t=e(((e,t)=>{
 /*!
 * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
 * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
